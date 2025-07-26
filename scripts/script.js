1c09200ff16fb3252e2eabf9554e15667178d729
Matheus Magnago <magnagomatheus7@gmail.com>
2025-07-26 15:09:14 -0300

    Treating the possibility of the user typing negative numbers or non-numeric characters and the number generation logic was completed

--- scripts/script.js
+++ scripts/script.js
@@ -7,26 +7,57 @@ const FORM = document.querySelector("form");
 
 let sortedNumbers = [];
 
+// Impede que o usuario digite algo diferente de um numero positivo.
+QTDNUM.onkeydown = (e) => {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+// Impede que o usuario digite algo diferente de um numero positivo.
+FROM.onkeydown = (e) => {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+// Impede que o usuario digite algo diferente de um numero positivo.
+TO.onkeydown = (e) => {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
 
 FORM.onsubmit = (event) => {
     event.preventDefault();
 
-    let max = TO.value;
-    let min = FROM.value;
-    let number;
+    try {
+        let max = TO.value;
+        let min = FROM.value;
+        let number;
 
-    for (i = 0; i < QTDNUM.value; i++) {
-        number = Math.floor(Math.random() * (max - min) + min);
-        if (TOGGLE.checked && sortedNumbers.length < ((max - min) + 1)) {
-            while(sortedNumbers.includes(number)) {
-                console.log("Este numero ja existe, sorteando outro!!!");
-                number = Math.floor(Math.random() * (max - min) + min) + 1;
+        for (i = 0; i < QTDNUM.value; i++) {
+            number = Math.floor(Math.random() * (max - min) + min);
+            if (TOGGLE.checked && sortedNumbers.length < ((max - min) + 1)) {
+                while(sortedNumbers.includes(number)) {
+                    console.log("Este numero ja existe, sorteando outro!!!");
+                    number = Math.floor(Math.random() * (max - min) + min) + 1;
+                }
+                sortedNumbers.push(number);
+            } else if(!TOGGLE.checked) {
+                sortedNumbers.push(number);
             }
-            sortedNumbers.push(number);
-        } else if(!TOGGLE.checked) {
-            sortedNumbers.push(number);
         }
+        console.log(sortedNumbers);
+    } catch(error) {
+        alert("Nao foi possivel sortear os numeros, tente novamente!");
+        console.log(error);
     }
-    console.log(sortedNumbers);
 }
-console.log(sortedNumbers);
